a17943c26d4c68aa1e147e47fff3a4292b33e648
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-10-15 18:23:30 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+main()
+{
+int i;
+for (i=1; i<=20; i++){
+putchar ('*');
+putchar('\n');}
+
+ puts("numeros de 1 ate 20 ");
+
+ for (i=1; i<=20; i++){
+     putchar ('*');
+     putchar ('\n');
+ }
+ for (i=1; i<=20; i++){
+ printf("%d\n",i);
+ }
+ for (i=1; i<=20; i++){
+     putchar('*');
+ }
+ }
+
+
